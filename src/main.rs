7d4072af947d6aede187968f7a097519b4b385b2
Chris Blatt <cablatt@mtu.edu>
2023-07-23 16:57:57 -0400

    added sample api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+/*
+Author: Chris Blatt
+Purpose: Backend to add DNS records to a db
+*/
+
+#![feature(decl_macro)]
+
+use rocket::get;
+use rocket::routes;
+use rocket::http::RawStr;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Navigate to http://localhost:8000/check/<type your Coin> to check  details about this crypto  !"
+}
+
+#[get("/check/<coin>")]
+fn check(coin: &RawStr) -> Result<String, Box<dyn std::error::Error>> {
+    let request_url =format!("https://api.coingecko.com/api/v3/coins/{}", coin);
+    println!("{}", request_url);
+    let resp = reqwest::blocking::get(request_url)?;
+    if resp.status().is_success(){
+
+        Ok(format!("{} ", resp.text().unwrap()))
+    }
+    else{
+        let response = resp.text().unwrap();
+        Ok(format!("{} is not a coin!", response))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, check]).launch();
 }
